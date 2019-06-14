b0ad9010844131c8c216166518407d7e8c9dccf7
Marwen94 <marwen.taleb94@gmail.com>
2019-06-14 16:10:08 +0200

    modification 14/06

--- contracts/Cv.sol
+++ contracts/Cv.sol
@@ -6,22 +6,36 @@ pragma solidity ^0.4.5;
 
 contract Cv {
 
+   uint80 constant None = uint80(0);
 	// d�finir les evts
 	struct Item {
-			uint8 item_type;             // 1 : Diplome, 2 : Exp�rience, 3 : Certification
+			uint8 item_type;             // 1 : Diplôme, 2 : Fiche de paie, 3 : Justificatif de domicile, 4 : document d'assurance
 			string nom;
 			string description;
 			string url;
+			uint date;
 			address validated_by;
 			string file_sha1;
 	}
 
+  struct User {
+
+    address user_address;
+    string  user_name;
+    uint8   user_type;     // 1: citoyen, 2: validation entity
+
+  }
+
 	address owner;
 	uint128 public itemCount;         //Nombre des docs
+  uint128 public userCount;         //Nombre des utilisateurs
 	Item[] public items;             //La liste de tout les docs
+  User[] public users;             //La liste de tout les utilisateurs
 	mapping (address => uint32) itemCountByUser;   //Nombre de docs par utilisateur
 	mapping (uint128 => address) userByItem; //itemIndex -> address
 	mapping (address => mapping (uint32 => uint128)) itemByIdByUser; //address -> itemIndexByUser -> itemIndex
+  mapping (address => uint128) usersIndex; //address to index_user
+
 
     modifier onlyBy(address _account)
     {
@@ -33,18 +47,56 @@ contract Cv {
         require(msg.sender == owner,"You must be the contract owner");
         _;
     }
+	//	modifier onlyValidateur()
+		//{
+		   // require(isValidateur(msg.sender),"You must be a validateur");
+	    //	_;
+	//	}
+
+	//	modifier onlyNormalUser()
+	//	{
+	//			require( !isValidateur(msg.sender),"You must be a normal user");
+	//			_;
+	//	}
 
     constructor() public {
         owner = msg.sender;
     }
 
+    //Ajouter un utilisateur en indiquant sa nature
+	function CreateUser(address _user_address, string  _user_name, uint8  _user_type) public returns (bool success) {
+				User memory newUser;
+        newUser.user_address = _user_address;
+        newUser.user_name = _user_name;
+        newUser.user_type = _user_type;
+        usersIndex[_user_address] = userCount; // add user to users mapping
+        users.push(newUser);
+        userCount++;
+        return true;
+		}
+
+  //getter of user
+  function getUser(address _user_address) external view returns(address,string, uint8 ){
+        User memory user;
+        user=users[usersIndex[_user_address]];
+        return (user.user_address , user.user_name , user.user_type);
+    }
+
+  //Test function
+  function getFirstUser() external view returns(address,string,uint8) {
+        User memory user;
+        user = users[0];
+        return (user.user_address , user.user_name , user.user_type);
+    }
+
 	// Enregistre un nouvel item
-	function createItem(uint8 _item_type, string _nom, string _description, string _url, string _file_sha1)  public returns (bool success) {
+	function createItem(uint8 _item_type, string _nom, string _description, string _url, string _file_sha1)  public  returns (bool success) { //onlyNormalUser
 		Item memory newItem;
 		newItem.item_type = _item_type;
 		newItem.nom = _nom;
 		newItem.description = _description;
 		newItem.url = _url;
+		newItem.date = now;
 		newItem.file_sha1 = _file_sha1;
 		items.push(newItem);
 
@@ -88,9 +140,15 @@ contract Cv {
 		        );
 	}
 
-	function validateItem(uint128 _index) external onlyOwner  {
-		//TODO : ce ne sera plus onlyOwner.
-	    require (_index < itemCount,"This Item doesn't exist");
+	//function isValidateur(address _address) public returns (bool){
+	//		if (users[_address].user_type == 2 ){
+	//				return true;
+		//	}
+		//	return false;
+	//}
+
+	function validateItem(uint128 _index) external {  // onlyValidateur
+	  require (_index < itemCount,"This Item doesn't exist");
 		items[_index].validated_by = msg.sender;
 	}
 }
--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,4 @@
+
 App = {
 	web3Provider: null,
 	contracts: {},
@@ -28,16 +29,40 @@ App = {
 			// Connect provider to interact with contract
 			App.contracts.Cv.setProvider(App.web3Provider);
 
-
 			// Load account data
 			web3.eth.getCoinbase(function(err, account) {
 			  if (err === null) {
+
 				App.account = account;
 						// TODO : j'ai dû ajouté ce default account (new version metamask?)
 						web3.eth.defaultAccount = account;
 				$("#accountAddress").html("Your Account: " + account);
+
 			  }
 			});
+
+			App.contracts.Cv.deployed().then(function(instance) {
+				cvInstance = instance;
+				account = App.account;
+				console.log(account);
+				return cvInstance.getUser(account);
+			}).then(function(user) {
+				console.log(user[0], user[1], user[2]);
+				var user_type=user[2];
+				if(user_type == 1) {
+					$("#identification").html("identifié comme un citoyen : " +  user[1] );
+				} else if (user_type == 2) {
+					$("#identification").html("identifié comme un valideur : " + user[1] );
+				}else{
+					console.log(user_type);
+					$("#identification").html("cet utilisateur n'est pas identifié !!!");
+				}
+			});
+
+			App.contracts.Cv.deployed().then(function(instance) {
+			  cvInstance = instance;
+				return cvInstance.getFirstUser();
+			}).then(function(user){console.log(user)});
 
 			return App.render();
 		});
@@ -48,16 +73,22 @@ App = {
 
 	render: function(page) {
 		$("#itemRow").empty();
+		$('#itemRow_Validation').empty();
 		$('#page_title').empty();
 		$("#page_content").empty();
 		$("#itemForm").hide();
+		$('#validationEntityForm').hide();
+    $('#itemRow_Validation').hide();
+    $('#identité_form').hide();
 		if (page == null) {
 			page = 'moncv'
 		}
 		var cvInstance;
 
 		if (page == 'moncv') {
-			$('#page_title').html('Mon CV');
+			$('#page_title').html('Mes documents');
+			$('#validationEntityForm').hide();
+			$('#itemRow_Validation').hide();
 			// Load contract data
 			App.contracts.Cv.deployed().then(function(instance) {
 				cvInstance = instance;
@@ -69,7 +100,8 @@ App = {
 						cvInstance.getItemsIdByUserIndex(numItem).then(function(itemIndex) {
 							//alert(App.account + " a l'item " + numItem + " qui a l'index : " + itemIndex);
 							cvInstance.getItemsByItemIndexForUser(itemIndex).then(function(item) {
-
+								//$("#itemRow").empty();
+								console.log(item);
 								var itemRow = $('#itemRow');
 								var itemTemplate = $('#itemTemplate');
 								itemTemplate.find('.card-header').text(item[1]);
@@ -77,20 +109,20 @@ App = {
 								itemTemplate.find('.item-description').text(item[2]);
 								itemTemplate.find('.item-type').text(item[0]);
 								if (item[5] == "0x0000000000000000000000000000000000000000") {
-									itemTemplate.find('.badge').text("");
-									itemTemplate.find('.item-validation').text("");
+								  itemTemplate.find('.badge').removeClass('badge-success');
+									itemTemplate.find('.badge').addClass('badge-danger');
+									itemTemplate.find('.badge').text("Not validated ");
+									itemTemplate.find('.item-validation').text("0x0000");
 								} else {
+									itemTemplate.find('.badge').removeClass('badge-danger');
+									itemTemplate.find('.badge').addClass('badge-success');
 									itemTemplate.find('.item-validation').text(item[5]);
 									itemTemplate.find('.badge').text("Validated");
 								}
 								//alert("ajout de la ligne avec titre : "+item[1]);
-
 								itemRow.append(itemTemplate.html());
-
 							});
-
 						});
-
 					}
 				} else {
 					$("#page_content").html("La blockchain est vide pour ce compte...");
@@ -102,6 +134,8 @@ App = {
 			});
 		} else if (page == 'viewAll') {
 			$('#page_title').html('Blockchain entière');
+			$('#validationEntityForm').hide();
+			$('#itemRow_Validation').hide();
 			// Load contract data
 			App.contracts.Cv.deployed().then(function(instance) {
 				cvInstance = instance;
@@ -110,6 +144,7 @@ App = {
 				if (itemCount != 0) {
 					for (itemIndex=0; itemIndex< itemCount; itemIndex++) {
 						cvInstance.getItemsByItemIndex(itemIndex).then(function(item) {
+							console.log("Im here");
 							var itemRow = $('#itemRow');
 							var itemTemplate = $('#itemTemplate');
 							itemTemplate.find('.card-header').text(item[2]);
@@ -123,34 +158,72 @@ App = {
 								itemTemplate.find('.item-validation').text(item[6]);
 								itemTemplate.find('.badge').text("Validated");
 							}
-
 							itemTemplate.find('.item-adresse').text(item[0]);
 							itemTemplate.find('.item-adresse-section').show();
-
 							itemRow.append(itemTemplate.html());
-
 						});
-
 					}
 				} else {
 					$("#page_content").html("La blockchain est vide pour ce compte...");
-
 				}
-
 			}).catch(function(error) {
 				console.warn(error);
 				$("#msg_danger").html("Impossible de se connecter au contrat dans blockchain!!!");
 				$("#msg_danger_template").show();
-				//alert("warning affiché");
 			});
 		} else if (page == 'additem') {
-			$('#page_title').html('Ajouter un item');
+			$('#validationEntityForm').hide();
+			$('#itemRow_Validation').hide();
+			$('#page_title').html('Ajouter un document');
 			$("#page_content").html("Formulaire de l'item");
 			$("#itemForm").show();
-		} else {
-			//alert("page inconnue "+page);
-		}
 
+		} else if (page == 'validationitems') {
+			$('#page_title').html('Valider un document');
+			$("#validationEntityForm").show();
+			$('#itemRow_Validation').show();
+			var validation_type = $('#type_Valideur').val();
+			App.contracts.Cv.deployed().then(function(instance) {    ///////
+				cvInstance = instance;
+				return cvInstance.itemCount();
+			}).then(function(itemCount) {
+				if (itemCount != 0) {
+					for (itemIndex=0; itemIndex< itemCount; itemIndex++) {
+						cvInstance.getItemsByItemIndex(itemIndex).then(function(item) {
+							//console.log(validation_type);
+							if (item[1] == validation_type) {
+								var itemRow = $('#itemRow_Validation');
+								var itemTemplate = $('#itemTemplate');
+								itemTemplate.find('.card-header').text(item[2]);
+								itemTemplate.find('img').attr('src', item[4]);
+								itemTemplate.find('.item-description').text(item[3]);
+								itemTemplate.find('.item-type').text(item[1]);
+							  itemTemplate.find('.item-adresse').text(item[0]);
+							  itemTemplate.find('.item-adresse-section').show();
+							  itemRow.append(itemTemplate.html());
+								//console.log(, typeof itemRow );
+							//	$("#page_content").html("Aucun document à valider pour ce valideur!");
+						} else {
+							$("#content2").html("Aucun document à valider pour cette entité!");
+						}});
+					}
+				}
+				}).catch(function(error) {
+					console.warn(error);
+					$("#msg_danger").html("Impossible de se connecter au contrat dans blockchain!!!");
+					$("#msg_danger_template").show();
+          });
+    } else if (page == 'identity') {
+
+			web3.eth.getCoinbase(function(err, account) {
+				if (err === null) {
+				App.account = account;
+						web3.eth.defaultAccount = account;
+				}
+				$('#identité_form').show();
+				document.getElementById('adresse_user').value = account;
+			});
+		}
 	},
 
 
@@ -161,7 +234,6 @@ App = {
 				cvInstance = instance;
 				//alert("avant valication de l'item " +item_id);
 				cvInstance.validateItem(item_id).then(function() {
-
 					//alert("ok validé");
 					return App.render('moncv');
 				}).catch(function(error) {
@@ -187,21 +259,43 @@ App = {
 				$("#nav_moncv").addClass('active');
 				$("#nav_addItem").removeClass('active');
 				$("#nav_viewAll").removeClass('active');
+				$("#nav_validation").removeClass('active');
+				$("#nav_identité").removeClass('active');
 				return App.render('moncv');
 			} else if (evt.target.id == "nav_addItem"){
 				$("#nav_moncv").removeClass('active');
 				$("#nav_addItem").addClass('active');
 				$("#nav_viewAll").removeClass('active');
+				$("#nav_validation").removeClass('active');
+				$("#nav_identité").removeClass('active');
 				return App.render('additem');
 			} else if (evt.target.id == "nav_viewAll"){
 				$("#nav_moncv").removeClass('active');
 				$("#nav_addItem").removeClass('active');
 				$("#nav_viewAll").addClass('active');
+				$("#nav_validation").removeClass('active');
+				$("#nav_identité").removeClass('active');
 				return App.render('viewAll');
+			} else if (evt.target.id == "nav_validation") {
+				$("#nav_validation").addClass('active');
+				$("#nav_moncv").removeClass('active');
+				$("#nav_addItem").removeClass('active');
+				$("#nav_viewAll").removeClass('active');
+				$("#nav_identité").removeClass('active');
+				return App.render('validationitems');
+			} else if (evt.target.id == "nav_identité") {
+				$("#nav_identité").addClass('active');
+				$("#nav_validation").removeClass('active');
+				$("#nav_moncv").removeClass('active');
+				$("#nav_addItem").removeClass('active');
+				$("#nav_viewAll").removeClass('active');
+				return App.render('identity');
 			} else if (evt.target.id == "search-button"){
 				$("#nav_moncv").removeClass('active');
 				$("#nav_addItem").removeClass('active');
 				$("#nav_viewAll").removeClass('active');
+				$("#nav_validation").removeClass('active');
+				$("#nav_identité").removeClass('active');
 				$("#itemRow").empty();
 				$('#page_title').empty();
 				$('#page_title').html('Validate item');
@@ -250,7 +344,6 @@ App = {
 				}).then(function(val) {
 					$("#msg_success").html("Nouvel item enregistré dans la blockchain!!!");
 					$("#msg_success_template").show();//("slow").delay(2000).hide("slow");
-
 					$("#nav_moncv").addClass('active');
 					$("#nav_addItem").removeClass('active');
 					return App.render('moncv');
@@ -260,16 +353,40 @@ App = {
 					$("#msg_danger").html("Impossible d'enregistrer l'item dans blockchain!!!");
 					$("#msg_danger_template").show();
 				});
-			} else {
-				//alert("clic on "+evt.target.id);
+			} else if (evt.target.id == "identité_submit") {
+				var address = $('#adresse_user').val();
+				var nom = $('#nom_user').val();
+				var type_user = $('#type_user').val();
+				console.log(address , nom , type_user);
+				App.contracts.Cv.deployed().then(function(instance) {
+					cvInstance = instance;
+					return cvInstance.CreateUser(address, nom, type_user);
+				}).then(function(val){
+
+					$("#msg_success").html("Nouvel utilisateur est enregistré!!!");
+					$("#msg_success_template").show();
+					if(type_user == 1){
+						$("#nav_addItem").addClass('active');
+						$("#nav_identité").removeClass('active');
+						return App.render('additem');
+					}else{
+						$("#nav_validation").addClass('active');
+						$("#nav_identité").removeClass('active');
+						return App.render('validationitems');
+					}
+				}).catch(function(error) {
+					console.warn(error);
+					$("#msg_danger").html("Impossible d'enregistrer cet utilisateur!!!");
+					$("#msg_danger_template").show();
+				});
 			}
 		});
-
 	},
 };
 
 $(function() {
 	$(window).on('load', function() {
+	//	App.use(express.static('src/upload_parser.php'));
 		App.init();
 	});
 });
